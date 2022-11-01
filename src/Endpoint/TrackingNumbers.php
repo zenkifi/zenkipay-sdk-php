8c52c23991f07d42ad27652fbb90c8c40c94487c
Federico Balderas <federico.balderas@zenki.fi>
2022-11-01 09:14:50 -0600

    Endpoints for disputes and tracking number were added

---
+++ src/Endpoint/TrackingNumbers.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zenkipay\Endpoint;
+
+use Zenkipay\HttpClient\Message\ResponseMediator;
+use Zenkipay\Sdk;
+
+final class TrackingNumbers
+{
+    private Sdk $sdk;
+
+    public function __construct(Sdk $sdk)
+    {
+        $this->sdk = $sdk;
+    }
+
+    public function create(array $data): object
+    {
+        return ResponseMediator::getContent($this->sdk->getHttpClient()->post('/v1/api/tracking', [], $data));
+    }
+}
